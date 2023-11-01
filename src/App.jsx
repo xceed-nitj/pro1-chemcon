09cbd56ecb43945fe14638314722ec9d149a7057
Akleem khan <akleemkhan0208@gmail.com>
2023-11-01 22:27:44 +0530

    Over Event

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Timeline from './components/Timeline'
 import Navbar from './components/Navbar'
+import OurEvents from './components/OurEvents'
 
 function App() {
   return (
     <>
       <Navbar />
       <Timeline />
+      <OurEvents />
     </>
   )
 }
