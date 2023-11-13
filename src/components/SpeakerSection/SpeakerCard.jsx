eb5cc45255f7dbf4a73e4cd6308d0a5f6c02921b
Akleem khan <akleemkhan0208@gmail.com>
2023-11-13 22:15:54 +0530

    fix navigation in swiper

--- src/components/SpeakerSection/SpeakerCard.jsx
+++ src/components/SpeakerSection/SpeakerCard.jsx
@@ -27,17 +27,37 @@ const speakers = [
         name: "Elon Musk",
         imageUrl: './speaker.jpeg',
     },
+    {
+        name: "Elon Musk",
+        imageUrl: './speaker.jpeg',
+    },
+    {
+        name: "Elon Musk",
+        imageUrl: './speaker.jpeg',
+    },
+    {
+        name: "Elon Musk",
+        imageUrl: './speaker.jpeg',
+    },
+    {
+        name: "Elon Musk",
+        imageUrl: './speaker.jpeg',
+    },
+    {
+        name: "Elon Musk",
+        imageUrl: './speaker.jpeg',
+    },
 ]
 function SpeakerCard() {
 
     return (
 
-        <Swiper className='py-10 max-w-5xl mx-auto px-10'
+        <Swiper className='md:py-10 max-w-5xl mx-auto px-10'
             spaceBetween={50}
             initialSlide={2}
             speed={300}
             slidesPerView={1}
-            pagination={{ clickable: true }}
+            pagination={{ clickable: true, dynamicBullets: true, }}
             navigation={true}
             loop={true}
             centeredSlides={true}
@@ -55,7 +75,7 @@ function SpeakerCard() {
                             <div className={`object-contain w-full h-full relative ease-out duration-300 delay-[250] 
                         ${isActive ? "scale-125" : "opacity-30"} `} >
                                 <img src={speaker.imageUrl} className={`object-contain w-full h-full rounded-xl`} />
-                                <div className='absolute bottom-0 bg-gradient-to-t from-30% via-70% from-black/50  w-full p-4 rounded-xl'>
+                                <div className='absolute bottom-0 bg-gradient-to-t from-40% via-90% from-black/60  w-full p-4 rounded-xl'>
                                     <h1 className='text-white font-semibold text-xl'>{speaker.name}</h1>
                                 </div>
                             </div>
