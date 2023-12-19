b63bbd7323324d6a716dfbdf128c764986dcdaa1
Gautam Singla <ggsingla777@gmail.com>
2023-12-19 15:17:38 +0530

    Setup GH actions

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pro1-chemcon/",
   plugins: [
     react(),
     eslintPlugin({
