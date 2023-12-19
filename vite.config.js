7199ff38cdd2a79c8996e3f83b32ab20df612c9b
Gautam Singla <ggsingla777@gmail.com>
2023-12-19 16:01:05 +0530

    vite-config-updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pro1-chemcon/",
   plugins: [
     react(),
     eslintPlugin({
