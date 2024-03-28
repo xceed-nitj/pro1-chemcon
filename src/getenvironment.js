a762bd4804ed750316bc826b1c8040f636fe7487
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2024-03-28 10:49:50 +0530

    Update getenvironment.js

--- src/getenvironment.js
+++ src/getenvironment.js
@@ -3,7 +3,7 @@ function getEnvironment() {
     const currentURL = window.location.href;
 //const development = 'http://localhost:8010';
     const production = 'https://nitjtt.onrender.com';
-  //  const nitjServer = 'https://xceed.nitj.ac.in';
+     const nitjServer = 'https://xceed.nitj.ac.in';
   //for time being using nijtt server
     if (currentURL.includes('localhost')) {
       return production;
@@ -11,7 +11,7 @@ function getEnvironment() {
       return production;
     } else {
       // Default to a specific environment or handle other cases
-      return production;
+      return nitjServer;
     }
   }
   
