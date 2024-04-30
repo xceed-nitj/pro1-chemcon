9834a255e1a910f29319c621c7060e6b24017b57
Avnash Kumar <avnashk.ee.21@nitj.ac.in>
2024-04-30 23:14:40 +0530

    merging with backend

--- src/components/AboutNITJ.jsx
+++ src/components/AboutNITJ.jsx
@@ -1,45 +1,51 @@
 import Separator from "./common/Separator";
+import axios from "axios";
+import getEnvironment from "../getenvironment";
+import { useState, useEffect } from "react";
 
-function AboutNITJ() {
 
 
+function AboutNITJ(props) {
+
+    const apiUrl = getEnvironment();
+
+    const confid = props.confid;
+    const [data, setData] = useState(null)
+    useEffect(() => {
+        axios.get(`${apiUrl}/conferencemodule/home/conf/${confid}`, {
+            withCredentials: true
+
+        })
+            .then(res => {
+                setData(res.data);
+                console.log(res.data);
+
+            })
+            .catch(err => console.log(err))
+
+    }, []);
 
     return (
-        <div className=" bg-white container flex flex-col items-center lg:flex-row lg:justify-between lg:items-start max-w-7xl  px-4 sm:px-10  lg:px-8 mx-auto pt-20">
+        <div className=" bg-white container flex flex-col items-center lg:flex-row lg:justify-between lg:items-start max-w-7xl  px-4 sm:px-10  lg:px-8 mx-auto pt-20 min-h-[280px]">
             <div className="w-full md:w-[700px] px-4 lg:w-3/5 lg:max-w-full">
                 <h2 className="text-4xl font-sans font-bold text-gray-950  ">
                     About NITJ        </h2>
                 <Separator />
-                <p className="text-base text-justify font-sans font-base min-h-[216px] text-gray-800">
-                    Dr. B. R. Ambedkar NIT Jalandhar was established in the year
-                    1987 as erstwhile Regional Engineering College and was given
-                    the status of NIT Jalandhar by the Ministry of Human Resource
-                    Development, Government of India, New Delhi on October 17,
-                    2002 under the Act of Parliament-2007.As an Institute of National importance, NIT Jalandhar
-                    comprehends its responsibility of providing high quality education
-                    in Engineering and Technology to produce competent technical
-                    manpower for the country. The Institute offers UG, PG and
-                    research programmes with latest research infrastructure in all
-                    disciplines. Institute holds 46th NIRF ranking and within a ranking
-                    band of 651-700 amongst Asian Universities.
+                <p className="text-base  text-justify font-sans font-base text-gray-800">
+
+                    {data ? <div dangerouslySetInnerHTML={{ __html: data.about[3].description }} /> : <div ></div>}
                 </p>
 
             </div>
             <div className="w-full md:w-[700px] lg:w-2/5 lg:max-w-[450px] px-4 mt-10 lg:mt-0 mx-auto lg:px-5 ">
-            
+
                 <h2 className="text-4xl font-sans font-bold  text-gray-950  ">
                     About the Department
                 </h2>
                 <Separator />
                 <p className="text-base  text-justify font-sans font-base text-gray-800">
-                    The Department (FIST Sponsored) was established in 1990 and
-                    has been playing a vital role in the development of Chemical
-                    Engineering education and research by offering B. Tech, M. Tech
-                    and PhD programmes. Its Alumni are occupying eminent positions
-                    in chemical industries, research and academic institutions in India
-                    and abroad. The Department has established state of the art
-                    laboratories with sophisticated equipment for undergraduate
-                    courses and research work.
+                    {data ? <div dangerouslySetInnerHTML={{ __html: data.about[4].description }} /> : <div ></div>}
+
                 </p>
             </div>
         </div>
--- src/components/AboutNews.jsx
+++ src/components/AboutNews.jsx
@@ -44,12 +44,12 @@ function AboutNews(props) {
   return (
     <div className=" bg-white container max-w-7xl flex flex-col items-center lg:flex-row lg:justify-evenly lg:items-start  mx-auto px-5 sm:px-10  lg:px-8 py-20">
       <div className="w-full  md:w-[700px]  s px-4 lg:w-3/5 ">
+
         <h2 className="text-4xl   font-sans font-bold text-gray-950  ">
-          CHEMCON 2024
-        </h2>
+          CHEMCON 2024        </h2>
         <Separator />
         <p className="text-base text-justify font-sans font-base text-gray-800">
-          {data ? <div className="min-h-[216px]" dangerouslySetInnerHTML={{ __html: data.aboutConf }} /> : <div className="min-h-[216px]"></div>}
+          {data ? <div className="min-h-[216px]" dangerouslySetInnerHTML={{ __html: data.about[0].description }} /> : <div className="min-h-[216px]"></div>}
 
         </p>
         <h2 className="text-4xl font-sans font-bold mt-4 text-gray-950  ">
@@ -57,7 +57,7 @@ function AboutNews(props) {
         </h2>
         <Separator />
         <p className="text-base text-justify font-sans font-base text-gray-800">
-          {data ? <div dangerouslySetInnerHTML={{ __html: data.aboutIns }} /> : " "}
+          {data ? <div dangerouslySetInnerHTML={{ __html: data.about[1].description }} /> : " "}
         </p>
       </div>
       <div className="sm:w-full md:w-[700px] sm:pr-6 lg:w-2/5 lg:ml-4 max-w-[700px] h-96 mt-10 sm:px-2 lg:mt-0 ml-5 md:mx-auto lg:px-5  ">
@@ -75,16 +75,32 @@ function AboutNews(props) {
             onMouseOut={() => setIsMouseOver(false)}
           // className="flex flex-col  animate-wiggle"
           >
+
             {newsData.map((item) => (
               <Link key={item._id} target="_blank" rel="noopener noreferrer"
                 to={item.link !== "" ? item.link : `/news/${item._id}`}  >
-                <div className="relative p-4 my-4 rounded-xl hover:shadow-md hover:shadow-accent-600/50 bg-white space-y-4 border-solid border-2 border-amber-400">
+
+                {!item.hidden && <div className="relative p-4 my-4 rounded-xl hover:shadow-md hover:shadow-accent-600/50 bg-white space-y-4 border-solid border-2 border-amber-400">
                   <div className="w-2 h-2 animate-ping bg-accent-600 absolute -left-1 -top-1 rounded-full"></div>
-                  <p className="text-base font-medium text-accent-600">{item.title}</p>
+                  <div className=' flex flex-row'>
+
+                    <p className="text-base font-medium text-accent-600">{item.title}                    </p>
+
+                    {item.new && <span className=' absolute mr-10 right-0  flex flex-row animate-bounce '>
+
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-5 stroke-amber-400 ">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                      </svg>
+                      <p className=" text-amber-400 text-sm ml-1 font-semibold">NEW</p>
+                    </span >}
+
+                  </div>
+
                   <p className="text-sm font-sans border-solid border-l-4 border-amber-400 pl-4">
                     {item.metaDescription}
                   </p>
                 </div>
+                }
               </Link>
             ))}
 
