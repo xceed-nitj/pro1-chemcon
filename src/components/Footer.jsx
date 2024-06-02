e207ac5125e58ea4d73ced5efbd53eadbcffe5c1
harimurugan1989 <harimur@gmail.com>
2024-06-02 09:35:29 +0530

    flag design updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -129,7 +129,7 @@ const Footer = () => {
             </div>
             <a href="https://info.flagcounter.com/1WSw">
                   <img
-                    src="https://s01.flagcounter.com/count2/1WSw/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_16/viewers_0/labels_1/pageviews_1/flags_0/percent_0/"
+                    src="https://s01.flagcounter.com/count2/SENi/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_VISITORS/labels_1/pageviews_1/flags_0/percent_0/"
                     alt="Flag Counter"
                     border="0"
                   />
